70abd218ebb3d89b420f7b5ea90934213bb87bff
iso11 <wiselsy60@gmail.com>
2015-07-29 17:59:54 +0900

    Update ExOh.rb

--- ExOh.rb
+++ ExOh.rb
@@ -1,21 +1,10 @@
 def ExOh(str)
 
   # code goes here
-  countx=0
-  counto=0
-  
-  str.each_char do |p|
-    if p=='x'
-      countx +=1
-    elsif p=='o'
-      counto +=1
-      end
-  end
-  
-  return countx==counto
+  return str.count('x')==str.count('o') 
          
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-ExOh(STDIN.gets)
+ExOh(STDIN.gets)  
